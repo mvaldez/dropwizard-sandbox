ec3b384d60bf32720916dad214d734f714bccbf7
mvaldez <markvaldez@gmail.com>
2016-11-21 21:07:55 -0600

    adding client resource; fixed build dependencies to match those used book; updated mysql driver version

--- src/main/java/com/sandbox/dropwizard/MyApplication.java
+++ src/main/java/com/sandbox/dropwizard/MyApplication.java
@@ -1,18 +1,26 @@
 package com.sandbox.dropwizard;
 
+import com.sandbox.resources.ClientResource;
 import com.sandbox.resources.ContactResource;
-import io.dropwizard.Application;
-import io.dropwizard.jdbi.DBIFactory;
-import io.dropwizard.setup.Environment;
-import org.skife.jdbi.v2.DBI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.skife.jdbi.v2.DBI;
+import io.dropwizard.jdbi.DBIFactory;
+import com.sun.jersey.api.client.Client;
+import io.dropwizard.client.JerseyClientBuilder;
 
 public class MyApplication extends Application<PhonebookConfiguration> {
-    public static final Logger LOGGER = LoggerFactory.getLogger(MyApplication.class);
+    static final Logger LOGGER = LoggerFactory.getLogger(MyApplication.class);
 
     public static void main(final String[] args) throws Exception {
         new MyApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<PhonebookConfiguration> b) {
     }
 
     @Override
@@ -25,5 +33,9 @@ public class MyApplication extends Application<PhonebookConfiguration> {
 
         // Add the resource to the environment
         environment.jersey().register(new ContactResource(jdbi, environment.getValidator()));
+
+        // build the client and add the resource to the environment
+        final Client client = new JerseyClientBuilder(environment).build("REST Client");
+        environment.jersey().register(new ClientResource(client));
     }
 }
